b27fdd828ae5bde8e5d6d0e5beab22a3825064d3
ylann bastian <ylann.bastian@gmail.com>
2026-02-13 15:33:13 +0100

    ajout du bouton de view et de selecttion dans les idref records + un peu de style

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -6,20 +6,23 @@ import { MainFacadeService } from './main-facade.service';
 import { LoadingIndicatorService } from '../services/loading-indicator.service';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
+	selector: 'app-main',
+	templateUrl: './main.component.html',
 })
 export class MainComponent implements OnInit {
+	public loader = inject(LoadingIndicatorService);
+	public facade = inject(MainFacadeService);
 
-  public loader = inject(LoadingIndicatorService);
-  public facade = inject(MainFacadeService);
+	public ngOnInit(): void {
+		this.facade.init();
+	}
 
-  public ngOnInit():void {
-    this.facade.init();
-  }
-  
-  public reset(): void {
-    this.facade.reset();
-  }
+	public openIdref(event: MouseEvent): void {
+		event.preventDefault(); // Empêche la navigation interceptée par l'host
+		window.location.assign('https://www.idref.fr/');
+	}
 
+	public reset(): void {
+		this.facade.reset();
+	}
 }
