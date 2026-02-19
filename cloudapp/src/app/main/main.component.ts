f30b8e4f59a3353f0ead670ccd543fc0683a31f6
ylann bastian <ylann.bastian@gmail.com>
2026-02-19 16:55:26 +0100

    ajout de l'initialisation de la configuration lors de la première connexion

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,21 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 // main.component.ts
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MainFacadeService } from './main-facade.service';
 import { LoadingIndicatorService } from '../services/loading-indicator.service';
+import { InitService } from '../services/init.service';
 
 @Component({
 	selector: 'app-main',
 	templateUrl: './main.component.html',
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 	public loader = inject(LoadingIndicatorService);
 	public facade = inject(MainFacadeService);
+	private initService = inject(InitService);
 
-	public ngOnInit(): void {
-		this.facade.init();
-	}
 
 	public openIdref(event: MouseEvent): void {
 		event.preventDefault(); // Empêche la navigation interceptée par l'host
